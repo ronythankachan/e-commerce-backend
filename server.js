3bf953f39e232184a9542b645e52e88b093f883f
rony thankachan <rony.mail2me@gmail.com>
2021-12-16 03:10:54 +0530

    adding morgan for request logging

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const morgan = require("morgan");
 const authRouter = require("./routes/auth.routes");
 const productRouter = require("./routes/product.routes");
 require("dotenv").config();
 require("./DBConnection.js")();
 app.use(cors());
 app.use(express.json());
+app.use(morgan("combined"));
 // Routes
 app.use("/auth", authRouter);
 app.use("/product", productRouter);
