ad91669de6362e986ceec2490daa0b4f6b694807
rony thankachan <rony.mail2me@gmail.com>
2021-12-19 09:01:17 +0530

    file upload code reformat

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -3,20 +3,31 @@ const util = require("util");
 const { uploadFile } = require("../helpers/aws-s3.helper");
 
 const unlinkFile = util.promisify(fs.unlink);
+// Add or update product information
 const saveProduct = async (req, res) => {
-  console.log(req.files.thumbnail[0]);
-  console.log(JSON.stringify(req.body));
-  await uploadFile(req.files.thumbnail[0]).then(async () => {
-    await unlinkFile(
-      "uploads/productimages/" + req.files.thumbnail[0].originalname
-    );
-  });
-  for (const file of req.files.images) {
-    await uploadFile(file).then(async () => {
-      await unlinkFile("uploads/productimages/" + file.originalname);
+  const thumnail = req.files.thumbnail[0];
+  await uploadFile(thumnail)
+    .catch((err) => {
+      return res
+        .status(500)
+        .send({ message: "Uploading thumbnail to amazon s3 failed", err: err });
+    })
+    .finally(async () => {
+      await unlinkFile("uploads/productimages/" + thumnail.originalname);
     });
+  for (const file of req.files.images) {
+    await uploadFile(file)
+      .catch((err) => {
+        return res.status(500).send({
+          message: "Uploading thumbnail to amazon s3 failed",
+          err: err,
+        });
+      })
+      .finally(async () => {
+        await unlinkFile("uploads/productimages/" + file.originalname);
+      });
   }
-  res.send("testing");
+  res.send({ message: "Product data saved successfully" });
 };
 const deleteProduct = (req, res) => {};
 
--- helpers/aws-s3.helper.js
+++ helpers/aws-s3.helper.js
@@ -7,19 +7,12 @@ const s3 = new AWS.S3({
 });
 
 const uploadFile = async (file) => {
-  console.log(file);
   const params = {
     Bucket: process.env.S3_BUCKET_NAME,
     Key: file.filename,
     Body: await fs.readFile(file.path),
   };
-  console.log(process.env.AWS_ACCESS_KEY);
-  console.log(process.env.AWS_SECRET_KEY);
-
-  console.log("file is", file);
-  const result = await s3.upload(params).promise();
-  console.log(result);
-  return result;
+  return await s3.upload(params).promise();
 };
 module.exports = {
   uploadFile,
