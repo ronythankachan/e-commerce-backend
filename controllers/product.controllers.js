40cc008a7da50b91efb1baa012af4ac89504fb98
rony thankachan <rony.mail2me@gmail.com>
2022-01-28 00:15:06 +0530

    log removed

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -24,14 +24,14 @@ const uploadToAws = async (req, res) => {
 // Add a new product
 const saveProduct = async (req, res) => {
   const product = req.body;
-  console.log(product);
   try {
     if (req.body._id) await Product.findByIdAndUpdate(product._id, req.body);
     else await Product.create(req.body);
     res.send({ message: "Product Saved successfully" });
   } catch (err) {
-    console.log(err);
-    res.status(500).send({ message: "Failed to add product", err: err });
+    res
+      .status(500)
+      .send({ message: "Failed to add product", err: err.message });
   }
 };
 
