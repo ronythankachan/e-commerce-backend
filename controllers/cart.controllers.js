98bff27147b3bc604c9a7f0338b8b2aaf494de00
rony thankachan <rony.mail2me@gmail.com>
2022-02-21 17:26:44 +0530

    add to cart and update cart values

--- controllers/cart.controllers.js
+++ controllers/cart.controllers.js
@@ -1,9 +1,23 @@
 const Cart = require("../models/cart.model");
 const addToCart = async (req, res) => {
   try {
-    const cart = await new Cart(req.body).save();
-    res.send(cart);
+    if (!req.body._id) {
+      const result = await Cart.create(req.body);
+      res.send(result);
+    } else {
+      console.log(req.body._id);
+      console.log(req.body.products);
+      const updatedResult = await Cart.findByIdAndUpdate(
+        req.body._id,
+        {
+          $set: { products: req.body.products },
+        },
+        { new: true }
+      );
+      res.send(updatedResult);
+    }
   } catch (err) {
+    console.log(err);
     res.status(500).send({ message: "Failed to update cart" });
   }
 };
